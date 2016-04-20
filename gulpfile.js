09fc7b386c088bbc7a186019e4f9fe7e4d47ffbe
Joris Griffioen <joris@peerby.com>
2016-04-20 20:25:05 +0200

    Set up gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+/*
+ *  Gulpfile
+ */
+var startOffset = new Date().getTime();
+console.log('Starting Gulp');
+
+// Modules
+var gulp = require("gulp");
+var nodemon = require("gulp-nodemon");
+
+var end = new Date().getTime()-startOffset;
+console.log('Modules loaded after '+end+' ms');
+
+/*
+ *  Main tasks
+ *  Use these externally.
+ */
+gulp.task('default', ['start']);
+
+gulp.task('start', function () {
+    nodemon({
+        script: 'app.js'
+        , ext: 'js'
+        , env: { 'NODE_ENV': 'development' }
+    });
+});
