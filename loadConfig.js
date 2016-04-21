3a8ba42158c31834f5f209d739f6290a563689c7
Joris Griffioen <joris@peerby.com>
2016-04-21 21:55:53 +0200

    Try a different config setup

--- loadConfig.js
+++ loadConfig.js
@@ -1,26 +1,8 @@
 var _ = require('lodash');
 var config = require('./config.json');
-var secrets = {};
-
-// Try local file for secrets, if none was found catch error and try the environment variables
-try {
-    secrets = require('./secrets.json');
-}
-catch (e) {
-    console.log(e);
-    secrets = {
-        token: process.env.TOKEN,
-        google: {
-            private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-            private_key: process.env.GOOGLE_PRIVATE_KEY
-        }
-    };
-}
+var secrets = require('./secrets.json');
 _.merge(config, secrets);
 
-console.log(config);
+console.log('Running with config: ', config);
 
-// Whoop, got config, GOGOGO!
 module.exports = config;
-
-// TODO: Find a less ridiculous solution for this
