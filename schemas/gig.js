2d17c9fb3d54610c31d74448a2c29e48a0903486
Joris Griffioen <jorisjgriffioen@gmail.com>
2016-05-07 21:46:27 +0200

    Finally fix whitespace bs

--- schemas/gig.js
+++ schemas/gig.js
@@ -21,15 +21,46 @@ var schema = new Schema(
     times: {
         type: String
     },
-    confirmed: {
-        type: Boolean
-    },
     backline: {
         type: String
     },
     comments: {
         type: String
+    },
+    confirmed: {
+        type: Boolean
     }
 });
 
+schema.pre('save', function(next) {
+
+    // Rewrite break/newline+whitespace to single \n character
+    console.log('pre',this.venue.address);
+    if(this.venue && this.venue.address) {
+        this.venue.address = cleanBreaks(this.venue.address);
+        this.venue.address = stripEndWhitespace(this.venue.address);
+    }
+    console.log('post',this.venue.address);
+    if(this.times) {
+        this.times = cleanBreaks(this.times);
+        this.times = stripEndWhitespace(this.times);
+    }
+    if(this.backline) {
+        this.backline = cleanBreaks(this.backline);
+        this.backline = stripEndWhitespace(this.backline);
+    }
+    if(this.comments) {
+        this.comments = cleanBreaks(this.comments);
+        this.comments = stripEndWhitespace(this.comments);
+    }
+    next();
+});
+
 module.exports = mongoose.model('Gig', schema);
+
+function cleanBreaks(string) {
+    return string && string.replace ? string.replace(/(\r\n\s*|\n\s*|\r\s*)/gm, '\n') : string;
+}
+function stripEndWhitespace(string) {
+    return string && string.replace ? string.replace(/\s$/, '') : string;
+}
