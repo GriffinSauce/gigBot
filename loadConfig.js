b64fe564b9c393d76dca21299f1125fb8dea4cab
Joris Griffioen <jorisjgriffioen@gmail.com>
2016-05-22 12:25:08 +0200

    Alias prod/production env

--- loadConfig.js
+++ loadConfig.js
@@ -4,6 +4,7 @@ var secrets = require('./secrets.json');
 _.merge(config, secrets);
 
 config.env = process.env.NODE_ENV;
+config.env = config.env === 'production' ? 'prod' : config.env;
 if(!config.env) {
     console.log('No env, defaulting to local');
     config.env = 'local';
